e28845f0db357a62edad9c8f823a320f57d03de0
Leonardo Pottmayer <pottmayerleonardo@gmail.com>
2023-12-31 08:50:54 -0300

    Created file for migration table util functions

---
+++ src/utils/migrationsTableUtils.ts
@@ -0,0 +1,5 @@
+export const insertMigrations = async (migrationName: string) => {};
+
+export const getMigrationsStatus = async (environmentName: string) => {};
+
+export const updateMigrationStatus = async (migrationId: number) => {};
